8b16f1c630a0eab51fcbe9122a9561e3f15d2bed
Svttam <hhh829370@gmail.com>
2024-05-12 14:20:25 +0300

    replace sqlite to postgresql

--- graphs/graphs/settings.py
+++ graphs/graphs/settings.py
@@ -71,13 +71,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'graphs.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER':'django_admin',
+        'PASSWORD': '123456789',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
 
